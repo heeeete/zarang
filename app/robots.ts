0bb8eca0a609e322e924fa5e0a09ae9e8ea8c5cf
heeeete <mkoui@naver.com>
2026-03-29 10:00:20 +0900

    feat: robots.txt에서 특정 경로에 대한 disallow 규칙 제거

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/write', '/me', '/messages', '/api', '/auth', '/login'],
     },
     sitemap: 'https://zarang.vercel.app/sitemap.xml',
   };
